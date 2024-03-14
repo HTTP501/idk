420ecbe3eec52bee0802ec5ce56876bfe299c19c
김영준 <dudwns020202@naver.com>
2024-03-14 16:42:44 +0900

    [S10P22A501-274-fe] feat : 쇼핑몰 화면 작성 및 결제페이지 네비게이터 구현

--- frontend/idk/src/navigations/PaymentStack.js
+++ frontend/idk/src/navigations/PaymentStack.js
@@ -1,17 +1,25 @@
-import * as React from 'react';
-import { FinishPayment, ShoppingMall } from '../screens/Payment'
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { FinishPayment, ShoppingMall } from "../screens/Payment";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 function ShopStack() {
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="ShoppingMall" component={ShoppingMall} options={{ headerShown: false }} />
-        <Stack.Screen name="FinishPayment" component={FinishPayment} options={{ headerShown: false }} />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ShoppingMall"
+        component={ShoppingMall}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="FinishPayment"
+        component={FinishPayment}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 }
 
--- frontend/idk/src/screens/Payment/FinishPayment.js
+++ frontend/idk/src/screens/Payment/FinishPayment.js
@@ -1,5 +1,72 @@
-const FinishPayment = () => {
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  Modal,
+  TextInput,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { Fontisto, MaterialIcons } from "@expo/vector-icons";
+import theme from "../../style";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-}
+const FinishPayment = ({ route }) => {
+  const FinishPaymentStyle = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: 20,
+    },
+    backBtn: {
+      flex: 3,
+      marginTop: 60,
+    },
+    productsInfoBox: {
+      flex: 2,
+    },
+    payMethodBox: {
+      flex: 2,
+    },
+    resultBox: {
+      flex: 3,
+    },
+  });
 
-export default FinishPayment
+  return (
+    <View style={FinishPaymentStyle.container}>
+      {/* 주소 및 받는 이 정보 부분 */}
+      <View style={FinishPaymentStyle.backBtn}>
+        <TouchableOpacity>
+          <MaterialIcons name="arrow-back-ios-new" size={36} color="black" />
+        </TouchableOpacity>
+        <Text>SSAFY로 받기</Text>
+        <Text>로컬에서 나의 이름을 가져올게요</Text>
+        <Text>로컬에서 나의 전화번호를 가져올게요</Text>
+        <Text>서울특별시 강남구 테헤란로 212 (역삼동 718-5번지)</Text>
+        <TextInput placeholder={"배송 시 요청사항"}></TextInput>
+      </View>
+
+      {/* 주문 상품 정보 부분 */}
+      <View style={FinishPaymentStyle.productsInfoBox}>
+        <Text>주문정보를 받을거에요</Text>
+      </View>
+
+      {/* 결제 수단 정보 부분 */}
+      <View style={FinishPaymentStyle.payMethodBox}>
+        <Text>결제수단을 받을거에요</Text>
+      </View>
+
+      {/* 결제 결과 정보 부분 */}
+      <View style={FinishPaymentStyle.resultBox}>
+        <Text>결제 결과를 받을거에요</Text>
+      </View>
+    </View>
+  );
+};
+
+export default FinishPayment;
