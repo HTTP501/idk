52462246c9e68c48374248b2e1a1d56178bf331d
gw07167 <gw07167@naver.com>
2024-03-20 13:58:38 +0900

    [#S10P22A501-464] feat : memberId 대신 Authentication 적용

--- backend/idk/src/main/java/com/ssafy/idk/domain/account/service/TransactionService.java
+++ backend/idk/src/main/java/com/ssafy/idk/domain/account/service/TransactionService.java
@@ -8,6 +8,7 @@ import com.ssafy.idk.domain.account.repository.AccountRepository;
 import com.ssafy.idk.domain.account.repository.TransactionRepository;
 import com.ssafy.idk.domain.member.domain.Member;
 import com.ssafy.idk.domain.member.repository.MemberRepository;
+import com.ssafy.idk.domain.member.service.AuthenticationService;
 import com.ssafy.idk.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +24,10 @@ public class TransactionService {
 
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
-    private final MemberRepository memberRepository;
+    private final AuthenticationService authenticationService;
 
-    public List<TransactionResponseDto> getTransaction(Long memberId) {
-        Member member = memberRepository.findById(memberId).get();
+    public List<TransactionResponseDto> getTransaction() {
+        Member member = authenticationService.getMemberByAuthentication();
         Account account = accountRepository.findByMember(member)
                 .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
 
