2c5a34404e9b596ceafff8f5522159e82124889e
김대원 <harry5414@naver.com>
2024-03-26 17:56:11 +0900

    [#588] : feat: 저금통 등록, 상세 API 연결

--- frontend/idk/src/screens/Main/DetailPocket/DetailSavingBox.js
+++ frontend/idk/src/screens/Main/DetailPocket/DetailSavingBox.js
@@ -10,69 +10,71 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  Modal
+  Modal,
+  Alert
 } from "react-native";
 // 컴포넌트들
 import theme from "../../../style";
-import DepositList from "../../../components/DepositList";
 import formattedNumber from "../../../components/moneyFormatter";
 // 화면 크기
 import { Dimensions } from "react-native";
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SCREEN_HEIGHT = Dimensions.get("window").height;
-import { useIsFocused } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 import Loading from "../../../components/Loading";
+import { getPiggyBankDetailAxios, deletePiggyBankAxios } from '../../../API/Saving'
 
 const DetailSavingBox = ({ navigation, route }) => {
   const pocketId = route.params.pocketId
   let [loading, setLoading] = useState(false);
+
+  // 저금통 입출금 데이터
+  const [arrayTransaction, setArrayTransaction] = useState(null);
+
+  // 저금통 잔액, id
+  const [balance, setBalance] = useState(null)
+  const [piggyBankId, setPiggyBankId] = useState(null)
+
+  // 저금통 상세 조회 Axios
+  const fetchPiggyBankDetail = () => {
+    getPiggyBankDetailAxios(
+      pocketId,
+      res => {
+        setArrayTransaction(res.data.data.arrayTransaction)
+        setBalance(res.data.data.balance)
+        setPiggyBankId(res.data.data.piggyBankId)
+      },
+      err => {
+        if (err.response.data.code === 'PB403') {
+          Alert.alert(
+            "저금통이 없습니다.",
+            "계좌 페이지로 이동합니다.",
+            [
+              {
+                text: "확인",
+                onPress: () => navigation.navigate("Main"),
+              },
+            ]
+          )
+        }
+      }
+    )
+  };
+
   useEffect(() => {
+    fetchPiggyBankDetail();
     setTimeout(() => {
       setLoading(true);
     }, 1000);
   }, []);
 
-  const date = new Date()
+  // 화면 포커싱 시 데이터 다시 가져오기
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchPiggyBankDetail();
+    }, [])
+  );
 
-    // 이체 데이터
-    let [deposit, setDeposit] = useState([
-      {
-        transactionId: 1,
-        transactionContent: "SSAFY",
-        transactionAmount: 1000000,
-        transactionBalance: 1283600,
-        type: "입금",
-        depositType: "월급",
-        transactionCreatedAt: "2024-03-03 12:00:01",
-      },
-      {
-        transactionId: 2,
-        transactionContent: "조용훈",
-        transactionAmount: 50000,
-        transactionBalance: 333600,
-        type: "출금",
-        depositType: "계좌이체",
-        transactionCreatedAt: "2024-03-01 12:00:01",
-      },
-      {
-        transactionId: 3,
-        transactionContent: "윤예빈",
-        transactionAmount: 50000,
-        transactionBalance: 383600,
-        type: "출금",
-        depositType: "계좌이체",
-        transactionCreatedAt: "2024-02-29 12:00:01",
-      },
-      {
-        transactionId: 4,
-        transactionContent: "최현기",
-        transactionAmount: 10000,
-        transactionBalance: 393600,
-        type: "출금",
-        depositType: "계좌이체",
-        transactionCreatedAt: "2024-03-03 12:00:01",
-      },
-    ]);
 
   return (
     <View className="flex-1">
@@ -83,7 +85,7 @@ const DetailSavingBox = ({ navigation, route }) => {
 
             {/* 로고 알람 */}
             <View className="px-10 mt-10 mb-2">
-              <Header navigation={navigation}/>
+              <Header navigation={navigation} piggyBankId={piggyBankId}/>
             </View>
 
             {/* 정보 */}
@@ -92,19 +94,19 @@ const DetailSavingBox = ({ navigation, route }) => {
                 <Image source={require("../../../../assets/logo/pig_black.png")} style={{ width: 60, height: 60 }} />
                 <Text className='text-2xl font-bold ml-5'>저금통</Text>
               </View>
-              <Text className='text-3xl font-bold mt-5'>6,085원</Text>
+              <Text className='text-3xl font-bold mt-5'>{ balance }원</Text>
               <Text className='text-base mt-3'>매달 월급일, 남은 잔고가 쌓인 돈이에요!</Text>
               
               <View className='flex-row justify-around my-6'>
                 <TouchableOpacity
                   style={styles.button}
-                  onPress={() => {navigation.navigate('MinusSavingBox', {change: true})}}
+                  onPress={() => {navigation.navigate('MinusSavingBox', {change: true, pocketId, balance})}}
                 >
                   <Text className='text-base'>+ 넣기</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                   style={styles.button}
-                  onPress={() => {navigation.navigate('MinusSavingBox', {change: false})}}
+                  onPress={() => {navigation.navigate('MinusSavingBox', {change: false, pocketId, balance})}}
                 >
                   <Text className='text-base'>- 빼기</Text>
                 </TouchableOpacity>
@@ -112,21 +114,129 @@ const DetailSavingBox = ({ navigation, route }) => {
             </View>
           </View>
 
-          {/* 이체내역 */}
-          <DepositList deposit={deposit} />
+          {/* 저금통 내역 */}
+          {arrayTransaction
+          ? <DepositList arrayTransaction={arrayTransaction} />
+          : null}
           <StatusBar style="auto" />
         </ScrollView>
       ) : <Loading/>}
     </View>
   );
 };
+
+// 저금통 내역
+const DepositList = function ({ arrayTransaction }) {
+  // 1. 날짜별 맵 생성
+  const dateMap = new Map();
+  for (const item of arrayTransaction) {
+    const date = item.createdAt.substring(0, 10); // 날짜 추출
+    if (!dateMap.has(date)) {
+      dateMap.set(date, []);
+    }
+    dateMap.get(date).push(item);
+  }
+
+  // 2. 맵을 리스트로 변환
+  const dateList = Array.from(dateMap.entries());
+  return (
+    <View>
+      {dateList.map((item,index) => {
+        return <DepositOnedayList item={item} key={index}/>;
+      })}
+    </View>
+  );
+};
+
+// 날짜
+const DepositOnedayList = function ({ item }) {
+  const date = item[0];
+  const dayitimes = item[1];
+  // console.log(date,dayitimes);
+  return (
+    <View>
+      <Text className="ml-8 mb-2 text-xs">{date}</Text>
+      {dayitimes.map((item,index) => {
+        return <DepositItem
+        item={item} 
+        key={index}
+        />
+      })}
+    </View>
+  );
+};
+
+// 날짜에 대한 아이템
+const DepositItem = function ({ item }) {
+  const date = new Date(item.createdAt);
+  // 시간 추출
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  // 12시간 형식으로 변환 (오전/오후 표시 없음)
+  const time = `${hours < 10 ? "0" + hours : hours}:${
+    minutes < 10 ? "0" + minutes : minutes
+  }`;
+  return (
+    <View className='flex-row gap-3 px-8 py-3'>
+      <View>
+        <Image source={require('../../../../assets/icons/money.png')} style={{ width: 50, height:50}}/>
+      </View>
+      <View className="flex-grow">
+        <Text>{item.content}</Text>
+        <Text className='text-xs'>{time}</Text>
+      </View>
+      <View className="items-end">
+        <Text
+          style={
+            item.content === "입금"
+              ? { color: theme["sky-basic"] }
+              : { color: "red" }
+          }
+        >
+          {formattedNumber(item.amount)}원
+        </Text>
+        <Text>{formattedNumber(item.balance)}원</Text>
+      </View>
+    </View>
+  );
+};
+
 // 헤더
-const Header = ({navigation}) => {
+const Header = ({navigation, piggyBankId}) => {
   const [showModal, setShowModal] = useState(false);
 
   // 저금통 해지 Axios
   const deleteDonPocket = () => {
-    setShowModal(true)
+    deletePiggyBankAxios(
+      piggyBankId,
+      res => {
+        Alert.alert(
+          "저금통 해지에 성공했습니다.",
+          "계좌 페이지로 이동합니다.",
+          [
+            {
+              text: "확인",
+              onPress: () => navigation.navigate("Main"),
+            },
+          ]
+        )
+      },
+      err => {
+        if (err.response.data.code === 'PB403') {
+          Alert.alert(
+            "저금통이 없습니다.",
+            "계좌 페이지로 이동합니다.",
+            [
+              {
+                text: "확인",
+                onPress: () => navigation.navigate("Main"),
+              },
+            ]
+          )
+        }
+      }
+    )
+    setShowModal(false)
   }
   const logo = require("../../../../assets/logo/color_idk_bank_logo.png");
   return (
@@ -139,7 +249,7 @@ const Header = ({navigation}) => {
         <Image source={logo} style={{ width: 90, resizeMode: "contain" }} />
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={deleteDonPocket}
+        onPress={() => setShowModal(true)}
       >
         <Text className='text-lg'>해지하기</Text>
       </TouchableOpacity>
