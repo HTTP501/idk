9f9d985914a87f367b29572601266f898ae66770
윤선아 <gw07167@naver.com>
2024-03-29 10:44:57 +0900

    [#S10P22A501-576] fix : Optional 빌드 에러 수정

--- backend/idk/src/main/java/com/ssafy/idk/domain/account/service/AccountService.java
+++ backend/idk/src/main/java/com/ssafy/idk/domain/account/service/AccountService.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @Service
