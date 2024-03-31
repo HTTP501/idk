d1f028259466010a6325b6e1299dd2bbf631e8dc
Yebin <ybyb99@naver.com>
2024-03-31 22:55:32 +0900

    [#S10P22A501-639] feat: 알람 기능 시험

--- frontend/idk/usePushNotification.js
+++ frontend/idk/usePushNotification.js
@@ -109,32 +109,3 @@ export default function App() {
     </View>
   );
 }
-
-// import MainApp from './src/MainApp'
-// import * as React from 'react';
-// import SignupStack from './src/navigations/SignupStack'
-// import AuthStack from './src/navigations/AuthStack'
-// import { navigationRef } from './src/navigations/AppNavigation'
-// import AccountStack from './src/navigations/AccountStack'
-// import Tab from './src/navigations/Tab'
-// import LocalAxios from './src/API/LocalAxios'
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// const Stack = createNativeStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer ref={navigationRef}>
-//       <Stack.Navigator>
-//         <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown: false }} />
-//         <Stack.Screen name="SignupStack" component={SignupStack} options={{ headerShown: false }} />
-//         <Stack.Screen name="AuthStack" component={AuthStack} options={{ headerShown: false }} />
-//         <Stack.Screen name="AccountStack" component={AccountStack} options={{ headerShown: false }} />
-//         <Stack.Screen name="Tab" component={Tab} options={{ headerShown: false }} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
