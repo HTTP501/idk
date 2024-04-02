0b65aceaa7588edc8f83976dddf7dde312898971
gw07167 <gw07167@naver.com>
2024-04-02 18:01:21 +0900

    [#S10P22A501-679] remove : FCM 관련 파일 모두 제거

--- backend/idk/src/main/java/com/ssafy/idk/domain/account/service/TransactionService.java
+++ backend/idk/src/main/java/com/ssafy/idk/domain/account/service/TransactionService.java
@@ -15,10 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
