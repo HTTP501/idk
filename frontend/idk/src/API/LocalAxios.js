6c426dc4610e2bb821126d56fbba5a072d871ee8
yebin <yebin4684@gmail.com>
2024-04-02 12:52:21 +0900

    [#S10P22A501-639] feat:자동이체 해지 비밀번호 연결

--- frontend/idk/src/API/LocalAxios.js
+++ frontend/idk/src/API/LocalAxios.js
@@ -6,7 +6,7 @@ import { navigate } from "../navigations/AppNavigation";
 // 내가 적은것
 export default function localAxios() {
   const instance = axios.create({
-    baseURL: "http://j10a501.p.ssafy.io:8081/api",
+    baseURL: "https://j10a501.p.ssafy.io/api",
     headers: {
       tmp: "application/json",
     },
