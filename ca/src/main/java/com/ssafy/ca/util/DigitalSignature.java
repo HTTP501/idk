d6848a0f1755d677422576aafd0af71daf050b5f
최현기 <gusrl123456@gmail.com>
2024-04-02 15:14:20 +0900

    Update DigitalSignature.java

--- ca/src/main/java/com/ssafy/ca/util/DigitalSignature.java
+++ ca/src/main/java/com/ssafy/ca/util/DigitalSignature.java
@@ -46,13 +46,26 @@ public class DigitalSignature {
     public static byte[] signData(byte[] data) {
 
         try {
-            String privateKeyPath2 = "src/main/resources/certs/ca_private.key";
-            URL resourceUrl  = DigitalSignature.class.getClassLoader().getResource("certs/ca_private.key");
-            File privateKeyFile = new File(resourceUrl.toURI());
-            String privateKeyPath = privateKeyFile.getAbsolutePath();
-            LOGGER.severe("상대경로 : " + privateKeyPath2);
-            LOGGER.severe("절대경로 : " + privateKeyPath);
+            // String privateKeyPath2 = "src/main/resources/certs/ca_private.key";
+            // URL resourceUrl  = DigitalSignature.class.getClassLoader().getResource("certs/ca_private.key");
+            // File privateKeyFile = new File(resourceUrl.toURI());
+            // String privateKeyPath = privateKeyFile.getAbsolutePath();
+            // LOGGER.severe("상대경로 : " + privateKeyPath2);
+            // LOGGER.severe("절대경로 : " + privateKeyPath);
             
+            InputStream inputStream = DigitalSignature.class.getClassLoader().getResourceAsStream("certs/ca_private.key");
+            File privateKeyFile = File.createTempFile("ca_private", ".key");
+            try (FileOutputStream outputStream = new FileOutputStream(privateKeyFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            }
+            String privateKeyPath = privateKeyFile.getAbsolutePath();
+
+
+
             // 파일 존재 여부 확인
             if (!Files.exists(Paths.get(privateKeyPath))) {
                 LOGGER.severe("개인 키 파일이 존재하지 않습니다: " + privateKeyPath);
