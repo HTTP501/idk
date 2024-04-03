c687a1cd8ce587507eb0ab450273a780d04218d0
조용훈 <johbrain@naver.com>
2024-04-03 17:54:20 +0900

    [S10P22A501-724] feat: 업데이트 트리거 SSE 메시지 전송

--- backend/idk/src/main/java/com/ssafy/idk/global/stream/controller/SystemDateController.java
+++ backend/idk/src/main/java/com/ssafy/idk/global/stream/controller/SystemDateController.java
@@ -4,6 +4,7 @@ import com.ssafy.idk.global.result.ResultCode;
 import com.ssafy.idk.global.result.ResultResponse;
 import com.ssafy.idk.global.stream.dto.SystemDateResponseDto;
 import com.ssafy.idk.global.util.TimeUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class SystemDateController {
     private final TimeUtil timeUtil;
 
 
+    @Operation(summary = "시스템 시간 조회")
     @GetMapping("")
     public ResponseEntity<ResultResponse> getSystemDate() {
         SystemDateResponseDto systemDateResponseDto = SystemDateResponseDto.of(timeUtil.getSystemDate());
