079ceac9d42139044a757e5f5962914fd73475a4
최현기 <gusrl123456@gmail.com>
2024-04-03 12:00:39 +0900

    [#S1022A501-663-be-card-api] feat : 통합인증, 청구예정금액 조회 로직 구현

---
+++ card/src/main/java/com/ssafy/card/domain/digitalsigniture/dto/request/AuthenticationRequestDto.java
@@ -0,0 +1,19 @@
+package com.ssafy.card.domain.digitalsigniture.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class AuthenticationRequestDto {
+
+    private String connectionInformation;
+    private String receiverOrgCode;
+    private String providerOrgCode;
+    private String clientId;
+    private String clientSecret;
+    private Map<String, String> consent;
+    private String signedConsent;
+}
